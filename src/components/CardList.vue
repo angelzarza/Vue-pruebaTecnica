ad5d058fb89dec023af9559eb8a89de9c668eca7
Angel <anzo3d@gmail.com>
2021-09-15 23:07:06 +0200

    Change name pacientes.json for db.json

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -3,7 +3,7 @@
     <v-card>
       <v-data-table
         :headers="headers"
-        :items="items"
+        :items="db"
         :items-per-page="5"
         class="elevation-1"
       ></v-data-table>
@@ -22,101 +22,28 @@ export default {
           text: "Nombre y Apellidos",
           value: "name",
         },
-        { text: "Clínica", value: "calories" },
-        { text: "Objetivo Tratamiento", value: "fat" },
-        { text: "Estado", value: "carbs" },
-        { text: "Acciones", value: "protein" },
-        { text: "Iron", value: "iron" },
+        { text: "Clínica", value: "email" },
+        { text: "Objetivo Tratamiento", value: "username" },
+        { text: "Estado", value: "phone" },
+        { text: "Acciones", value: "website" },
       ],
-      items: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%",
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%",
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%",
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%",
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%",
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%",
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%",
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%",
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%",
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%",
-        },
-      ],
+      // items: [
+      //   {
+      //     name: "Frozen Yogurt",
+      //     calories: 159,
+      //     fat: 6.0,
+      //     carbs: 24,
+      //     protein: 4.0,
+      //     iron: "1%",
+      //   },
+      // ],
     };
   },
 
   mounted: function () {
     const axios = require("axios");
     axios
-      .get("https://my-json-server.typicode.com/typicode/demo/db")
+      .get("https://my-json-server.typicode.com/angelzarza/prueba-tecnica")
       .then((response) => {
         this.db = response.data;
         console.log(response);
